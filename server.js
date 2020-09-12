dea9629f384e0a18141bfa5d6bd2980f0655b591
nkoshkarov <koshkarovnik@gmail.com>
2020-09-13 01:31:17 +0300

    yarn.lock upd

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path               = require('path');
 const port               = process.env.PORT || 5000;
 const app                = express();
 
-app.use('/', express.static(path.join(__dirname, './dist/ui')));
+app.use('/', express.static(path.join(__dirname, './dist/traktrain-widget')));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, './dist/ui', 'index.html'));
+  res.sendFile(path.join(__dirname, './dist/traktrain-widget', 'index.html'));
 });
 
 app.listen(port, () => console.log('Api live on port', + port));
